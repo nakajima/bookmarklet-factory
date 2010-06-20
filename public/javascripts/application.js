db7169b27d1d06c4fa84520a2a4a75b734fec296
Pat Nakajima <patnakajima@gmail.com>
2010-06-20 16:22:05 -0400

    STYLECITY

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,37 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 function showTheCodes() {
-  $('source').appear({ duration: 0.3 });
+  var source = $('source');
+  var wrapper = $('source').wrap('div');
+  wrapper.hide();
+  source.show();
+  source.addClassName('visible');
+  wrapper.slideDown({
+    duration: 0.2,
+    afterFinish: function() {
+      wrapper.replace(source)
+    }
+  });
+}
+
+function hideTheCodes() {
+  var source = $('source');
+  var wrapper = $('source').wrap('div');
+  wrapper.slideUp({
+    duration: 0.2,
+    afterFinish: function() {
+      wrapper.replace(source)
+      source.removeClassName('visible');
+      source.hide();
+    }
+  });
+}
+
+function toggleTheCodes() {
+  var source = $('source');
+  if (source.hasClassName('visible')) {
+    hideTheCodes();
+  } else {
+    showTheCodes();
+  }
 }
